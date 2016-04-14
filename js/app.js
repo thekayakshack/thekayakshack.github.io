78054a92e0b89c77b885e4a915cae8aef5317d5f
Brian Getting <brian@tatem.ae>
2016-04-14 12:03:07 -0700

    Fixing polaroids

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $('.memories').find('.polaroid').each(function(i){
-    $polaroid = $(this);
+    var $polaroid = $(this);
 
     // Attach the frame (overflow hidden)
-    frame = document.createElement('div');
-    $frame = $(frame);
+    var frame = document.createElement('div');
+    var $frame = $(frame);
     $frame.addClass('frame').appendTo($polaroid);
 
     // Attach the image
-    image = document.createElement('img');
+    var image = document.createElement('img');
     $(image).attr('src', $polaroid.data('src')).appendTo($frame).load(function(){
       // Trigger developer to fade
       // Random delay, random length
